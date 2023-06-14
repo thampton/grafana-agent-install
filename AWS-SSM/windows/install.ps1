b9ec419dca96a19331192e35bfbe8c20dbe8eb22
Travis Hampton <travis@travishampton.net>
2023-06-14 09:20:14 -0500

    Update install.ps1

--- AWS-SSM/windows/install.ps1
+++ AWS-SSM/windows/install.ps1
@@ -1,5 +1,9 @@
 Write-Output 'Installing Grafana Agent...'
+$MYPARM1=$env:SSM_MYPARM1
+$MYPARM2=$env:SSM_MYPARM2
+$MYPARM3=$env:SSM_MYPARM3
+
 $grafana_agent_installer = @(Get-ChildItem grafana-agent-installer.exe)[0]
-Start-Process $grafana_agent_installer /S /EnableExporter true /Username <username> /Password <password> /Url "http://example.com"
+Start-Process $grafana_agent_installer /S /EnableExporter true /Username $MYPARM1 /Password $MYPARM2 /Url "http://example.com"
 Remove-Item $grafana_agent_installer -Force -Confirm:$false
 Write-Output 'Done'
